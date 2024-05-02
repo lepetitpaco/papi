292c3c5ace11de3f1a8cf28c956de807dcf2a5ae
lepetitpaco <daviphone121@gmail.com>
2024-05-02 19:16:40 +0000

    :sparkles: Created spendings table and renamed fields after error on creation

---
+++ database/migrations/2024_05_02_185629_create_spendings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpendingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spendings', function (Blueprint $table) {
+            $table->id();
+            $table->string('categorie');
+            $table->string('intitule');
+            $table->date('date');
+            $table->decimal('montant', 10, 2);
+            $table->boolean('preleve');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spendings');
+    }
+}
